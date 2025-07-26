867cd9450e2e0628e88304e007c1d29937417624
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2025-07-27 02:55:57 +0530

    updated & fixed validations using GlobalExceptionHandler and implemented pagination.

--- src/main/java/com/example/cinemahub2/repository/user/MovieRepo.java
+++ src/main/java/com/example/cinemahub2/repository/user/MovieRepo.java
@@ -1,8 +1,12 @@
 package com.example.cinemahub2.repository.user;
 
 
+import com.example.cinemahub2.DTO.MovieTitleDTO;
 import com.example.cinemahub2.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +15,7 @@ import java.util.Optional;
 public interface MovieRepo extends JpaRepository<Movie,Integer> {
     List<Movie> findTop5ByOrderByMovieIdDesc();
     Optional<Movie> findByMovieName(String movieName);
+
+    @Query("SELECT new com.example.cinemahub2.DTO.MovieTitleDTO(m.movieName, m.movieId) FROM Movie m")
+    Page<MovieTitleDTO> findAllMovieTitles(Pageable pageable);
 }
