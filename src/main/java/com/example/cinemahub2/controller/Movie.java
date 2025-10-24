b08d34aa009bc43be1fb3a402bbfe1080d6ac02e
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2025-10-24 21:30:04 +0530

    Revert "fix: Implement Request/Response Logging & API Versioning"

--- src/main/java/com/example/cinemahub2/controller/Movie.java
+++ src/main/java/com/example/cinemahub2/controller/Movie.java
@@ -5,18 +5,16 @@ import com.example.cinemahub2.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/movies")
 public class Movie {
 
     @Autowired
     private MovieService movieService;
 
 
-    @GetMapping("/{movieName}/shows")
+    @GetMapping("/movies/{movieName}/shows")
     public MovieWithShowsDTO getMovieShows(@PathVariable String movieName) {
         return movieService.getMovieWithShowDetails(movieName); // ✅ correct return type
     }
